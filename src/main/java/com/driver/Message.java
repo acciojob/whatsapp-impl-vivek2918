c1b70aaf7c940384f8a8f0ae9c848950b5878d60
unknown <chovatiyavivek1999@gmail.com>
2023-01-28 20:46:11 +0530

    Sol 2

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -10,6 +10,17 @@ public class Message {
     public Message() {
     }
 
+    public Message(int id) {
+        this.id = id;
+        this.timestamp = new Date();
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
